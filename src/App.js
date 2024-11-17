a035575159fb77c4b342075d4751ba3cbaf174e1
brian <brianjohnkiama@gmail.com>
2024-11-17 02:11:15 -0800

    props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 
-// we can add a new component in here
-// below is our person component
-
-const Person = () => {
+// every component has a built in props object
+const Person = (props) => {
   return (
     <>
-      <h1>Name: John</h1>
-      <h2>Last Name: Doe</h2>
-      <h2>Age: 30</h2>
+      <h1>Name: {props.name}</h1>
+      <h2>Last Name: {props.LastName}</h2>
+      <h2>Age: {props.age}</h2>
     </>
   )
 }
@@ -18,9 +16,10 @@ const App = () => {
   
   return (
     <div className="App">
-      <Person />
-      <Person />
-      <Person />
+      {/* below we are assigning a name to our first component as john */}
+      <Person name={'john'} LastName={'Doe'} age={25}/>
+      <Person name='victor' LastName='ketch' age={12}/>
+      <Person name="brian" LastName="Kiama" age={30}/>
     </div>
   );
 }
