6a6c0ce7a03ff3e419ecf0da726e1eedd1cf257c
brian <brianjohnkiama@gmail.com>
2024-11-17 05:42:13 -0800

    Events

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-// before we use state we need to import it
+// Event is an action that can be triggered as a result of the user action or some kind of system generated event
 import {useState} from 'react';
 import './App.css';
 
 
 
 const App = () => {  
-  // use const and use the concept of array destructuring that is how state works
-  // whenever you call something as a function and it starts with use, in react we cal that a hook
-  // in the square brackets we will have the name of the state
-  // the second part is going to be the setter function 
-  // a good rule of thumb is that you call the second variable the same as the first one but add the set infront because it is a setter function for the first variable
-  // inside of the usestate you provide the initial value
   const [counter, setCounter] = useState(0);
   
   return (
     <div className="App">
-      {/* to really show the purpose of state let's use a counter */}
-      <button>-</button>
+      {/* add an onclick property to below button as part of event handling */}
+      {/* in it we have a callback function */}
+      {/* a callback is the one that doesn't have a name , it's just waiting for some kind of a command*/}
+      {/* when we click the button we want setCounter function to be called */}
+      {/* we want when - button is clicked our counter is lowered by one thus inside the setCounter we create a callback function */}
+      {/* prevCount is just a good way of naming the parameter of the setCounter */}
+      {/* we then call the prevCount and decrement it by 1 */}
+      <button onClick={() => setCounter((prevCount) => prevCount -1)}>-</button>
       <h1>{counter}</h1>
-      <button>+</button>
+      <button onClick={() => setCounter((prevCount) => prevCount +1)}>+</button>
       
     </div>
   );
