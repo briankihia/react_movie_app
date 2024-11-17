2ba51a6a5d67f9faa3d4ba98a3d3b28c19ccfefe
brian <brianjohnkiama@gmail.com>
2024-11-17 01:29:18 -0800

    fragments

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
 
 const App = () => {
+  // we explore dynamic nature of react
+  // we can call different variables here
+  
+  const name = null;
   return (
     <div className="App">
-      <h1>Hello, react!</h1>
+      <h1>Hello, {name}!</h1>
+      {/* we open up curly braces and check if name exists. In that case we want to render this react fragment inside of this parenthesis*/}
+      {/* below is an if condition thus has 2 parts */}
+      {/* NB- if you want to render 2 different elements one next to another, you need to wrap them in a so called react fragments */}
+      {name ? (
+        <>tests</>
+      ): (
+        <>
+          <h1>test</h1>
+          <h2>no name available</h2>
+        </>
+        
+      )}
     </div>
   );
 }
