33282988ec397dffbb741a19ad1f25be8dbab7d1
brian <brianjohnkiama@gmail.com>
2024-11-17 01:59:46 -0800

    components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import './App.css';
 
-const App = () => {
-  // we explore dynamic nature of react
-  // we can call different variables here
+// we can add a new component in here
+// below is our person component
+
+const Person = () => {
+  return (
+    <>
+      <h1>Name: John</h1>
+      <h2>Last Name: Doe</h2>
+      <h2>Age: 30</h2>
+    </>
+  )
+}
+
+
+const App = () => {  
   
-  const name = null;
   return (
     <div className="App">
-      <h1>Hello, {name}!</h1>
-      {/* we open up curly braces and check if name exists. In that case we want to render this react fragment inside of this parenthesis*/}
-      {/* below is an if condition thus has 2 parts */}
-      {/* NB- if you want to render 2 different elements one next to another, you need to wrap them in a so called react fragments */}
-      {name ? (
-        <>tests</>
-      ): (
-        <>
-          <h1>test</h1>
-          <h2>no name available</h2>
-        </>
-        
-      )}
+      <Person />
+      <Person />
+      <Person />
     </div>
   );
 }
