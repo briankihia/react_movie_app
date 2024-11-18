b2788cea67e296674ec9cb648a554101274495e9
brian <brianjohnkiama@gmail.com>
2024-11-18 03:48:31 -0800

    upto this point the api is able to access the site and return data

--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
-// Event is an action that can be triggered as a result of the user action or some kind of system generated event
-import {useState, useEffect} from 'react';
-import './App.css';
+import React from "react";
+import { useEffect } from "react";
+
+// 6dd5f483
 
 
+// we explore the ways in which we can call this api to get all the data about movies
 
-const App = () => {  
-  const [counter, setCounter] = useState(0);
+// we create a static variable
+// we use the below inside of our component to gather the data
+// we want to fecth the data from this api as soon as our component loads thus we use the useEffect hook
+const API_URL = 'http://www.omdbapi.com?apikey=6dd5f483'
 
-  // useEffect accepts a callback function as a parameter
-// useEffect allows us to do something on some kind of event
-// below code will run as soon as the page loads. Th code happens as soon as this whole component renders
-  useEffect(() => {
-    // we want whenever counter is changed we want to get an alert
-    alert("You've changed counter to" + counter);
+// we create our app which is our main functional component
+const App = () => {
 
-  }, [counter]);
-  
-  return (
-    <div className="App">
-     
-      <button onClick={() => setCounter((prevCount) => prevCount -1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount +1)}>+</button>
-      
-    </div>
-  );
+     // inside here we call a function that is going to fetch our movies
+        // we use an async function which means it takes some time to fetch this movies 
+        // the async function takes an argument of title
+        // after finishing working on seachmovies we need to call it for the data to be seen
+        const searchMovies = async (title) => {
+            // the below is going to call our api
+            // The await keyword pauses the execution of the function until the Promise is resolved or rejected.
+            const response = await fetch(`${API_URL}&s=${title}`);
+
+            // once we get the response we have to get the data from it
+            const data = await response.json();
+
+            // inside this data object we should have the data about the movies
+            // we only need the movies array so we console data.search
+            console.log(data.Search);
+
+        }
+
+    // we use useEffect to fetch the data from our above api as soon as the component loads
+    useEffect(() => {
+    //    now we call searchMovies to see it's content
+    searchMovies('Spiderman');
+
+    }, []);
+
+    return (
+        <h1>App1</h1>
+
+    );
 }
 
+// we have to export every single one of our component
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
+// always start by importing the 2 below
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+// ReactDOM.render was replaced with createRoot thus add below import
+import {createRoot} from 'react-dom/client';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// below is going to trigger our div with an id of root and is going to inject our entire application right inside of it
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
